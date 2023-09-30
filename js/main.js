364370ec1c84b2d37431b64976c1880a3d2bb3be
VedantCMS <vedantgupta.cms@gmail.com>
2023-09-30 11:46:06 +0530

    sass created

--- js/main.js
+++ js/main.js
@@ -6,27 +6,4 @@ jQuery(window).scroll(function () {
        }
      });
 
-     jQuery(document).ready(function () {
-        jQuery(".toggle-me-now").click(function () {
-            jQuery(this).toggleClass("collapse-now");
-        });
-    });
-
-     $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:30,
-      autoplay:true,
-      nav:true,
-      dots:false,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          }
-      }
-  })
+     
