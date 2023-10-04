33ac7b53d41fa8e5bfcceec7bf321d8669ecc104
VedantCMS <vedantgupta.cms@gmail.com>
2023-10-04 17:01:06 +0530

    pushd noew

--- js/main.js
+++ js/main.js
@@ -6,4 +6,26 @@ jQuery(window).scroll(function () {
        }
      });
 
-     
+     (function($) { 
+      $(function() { 
+    
+        $('#navbar-toggle').click(function() {
+          $('nav ul').slideToggle();
+        });
+    
+        $('#navbar-toggle').on('click', function() {
+          this.classList.toggle('active');
+        });
+    
+        $('nav ul li a:not(:only-child)').click(function(e) {
+          $(this).siblings('.navbar-dropdown').slideToggle("slow");
+    
+          $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+          e.stopPropagation();
+        });
+    
+        $('html').click(function() {
+          $('.navbar-dropdown').hide();
+        });
+      }); 
+    })(jQuery); 
