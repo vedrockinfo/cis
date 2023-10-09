31b9c2011eda85eb41929d80ced9f3649251c2e5
VedantCMS <vedantgupta.cms@gmail.com>
2023-10-09 10:52:03 +0530

    headerupdated

--- js/main.js
+++ js/main.js
@@ -6,32 +6,6 @@ jQuery(window).scroll(function () {
   }
 });
 
-// Navbar 
-
-(function ($) {
-  $(function () {
-
-    $('#navbar-toggle').click(function () {
-      $('nav ul').slideToggle();
-    });
-
-    $('#navbar-toggle').on('click', function () {
-      this.classList.toggle('active');
-    });
-
-    $('nav ul li a:not(:only-child)').click(function (e) {
-      $(this).siblings('.navbar-dropdown').slideToggle("slow");
-
-      $('.navbar-dropdown').not($(this).siblings()).hide("slow");
-      e.stopPropagation();
-    });
-
-    $('html').click(function () {
-      $('.navbar-dropdown').hide();
-    });
-  });
-})(jQuery);
-
 
 $('.owl-carousel').owlCarousel({
   loop: true,
